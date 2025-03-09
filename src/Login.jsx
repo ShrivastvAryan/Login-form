e6a5c2a6f8a367bca1ce6102c8fcab0f9147a579
Aryan Shrivastava <asguy29@gmail.com>
2025-03-09 13:43:03 +0530

    added props

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,16 +1,40 @@
+import { useState } from "react"
+
 export const LoginForm=()=>{
+
+    const[user,setUser]=useState({
+        email:"",
+        password:""
+    });
+
+    const handleInput=(e)=>{
+        let name=e.target.name;
+        let value=e.target.value;
+
+        setUser({
+            ...user,
+            [name]:value,
+        })
+    };
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(user);
+    }
     return(
         <>
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-amber-700 w-3/5 h-3/5 rounded-sm">
         <p className="text-white p-2 text-4xl text-center">Login Form</p>
-        <form>
+        <form onSubmit={handleSubmit}>
            <div>
                     <label htmlFor="username" className=" text-white text-2xl p-2">Username or Email</label>
-                    <input type="text" name="username"
+                    <input type="text" name="email"
                      placeholder="username"
                      required
                      autoComplete="off"
-                    className=" m-2 bg-blue-100 p-1 rounded-md w-3/5"/>
+                    className=" m-2 bg-blue-100 p-1 rounded-md w-3/5"
+                    value={user.email}
+                    onChange={handleInput}/>
            </div>
 
            <div>
@@ -19,7 +43,9 @@ export const LoginForm=()=>{
                      placeholder="password"
                      required
                      autoComplete="off"
-                    className=" m-2 bg-blue-100 p-1 rounded-md"/>
+                    className=" m-2 bg-blue-100 p-1 rounded-md"
+                    value={user.password}
+                    onChange={handleInput}/>
             </div>
 
             <button type="submit" className="bg-blue-300 p-1 text-xl m-6 rounded-md cursor-pointer">Login</button>
